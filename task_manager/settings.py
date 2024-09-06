960332e4f1fe723972ed24a8d9a635c1eae8d5e3
Alfredo Barron <alfredobarronc@gmail.com>
2024-09-06 14:23:35 -0600

    change user

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         # 'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'task_manager',
-        'USER': 'postgres',
+        'USER': 'alfredobarron',
         'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432',
